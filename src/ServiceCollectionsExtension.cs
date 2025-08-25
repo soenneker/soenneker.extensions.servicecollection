b0f8353dbfe3e1e9bf1d5761b9765134164ff07f
Jake Soenneker <jake@soenneker.com>
2025-08-25 13:46:24 -0500

    AddArrClientCertForwarding

--- src/ServiceCollectionsExtension.cs
+++ src/ServiceCollectionsExtension.cs
@@ -1,9 +1,12 @@
-﻿using System.Text.Json.Serialization;
-using Asp.Versioning;
+﻿using Asp.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using Soenneker.Extensions.String;
+using System;
+using System.Buffers;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
 
 namespace Soenneker.Extensions.ServiceCollection;
 
@@ -57,11 +60,41 @@ public static class ServiceCollectionsExtension
     public static void ConfigureVersioning(this IServiceCollection services)
     {
         services.AddApiVersioning(o =>
+        {
+            o.DefaultApiVersion = new ApiVersion(1, 0);
+            o.ApiVersionReader = new HeaderApiVersionReader("api-version");
+            o.AssumeDefaultVersionWhenUnspecified = true;
+        });
+    }
+
+    /// <summary>
+    /// Adds certificate forwarding that reads a base64-encoded DER cert from a header (default: "X-ARR-ClientCert").
+    /// </summary>
+    public static IServiceCollection AddArrClientCertForwarding(this IServiceCollection services, string headerName = "X-ARR-ClientCert")
+    {
+        return services.AddCertificateForwarding(o =>
+        {
+            o.CertificateHeader = headerName;
+            o.HeaderConverter = headerValue =>
             {
-                o.DefaultApiVersion = new ApiVersion(1, 0);
-                o.ApiVersionReader = new HeaderApiVersionReader("api-version");
-                o.AssumeDefaultVersionWhenUnspecified = true;
-            }
-        );
+                if (headerValue.IsNullOrWhiteSpace())
+                    return null!;
+
+                int max = checked(headerValue.Length * 3 / 4);
+                byte[] rented = ArrayPool<byte>.Shared.Rent(max);
+
+                try
+                {
+                    if (!Convert.TryFromBase64String(headerValue, rented, out int _))
+                        return null!;
+
+                    return X509CertificateLoader.LoadCertificate(rented);
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(rented);
+                }
+            };
+        });
     }
 }
